33577dd0c78972edb098e021b1546ff0d31f1aab
Seker <sekermac@gmail.com>
2024-01-16 10:07:37 +0100

    Update 1

---
+++ database.js
@@ -0,0 +1,16 @@
+// database.js
+
+const users = [
+    {
+        id: 1,
+        username: "Seker",
+        password: "123",
+        // other user data...
+        id: 2,
+        username: "Marco",
+        password: "321",
+    }
+];
+
+module.exports = users;
+
